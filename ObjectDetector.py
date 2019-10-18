e325c190f71d506f8358c0e6a8bfce32e5881b5b
zychen2016 <zeyuchen2016@gmail.com>
2019-10-18 20:15:30 +0800

    Update ObjectDetector.py

--- ObjectDetector.py
+++ ObjectDetector.py
@@ -46,7 +46,7 @@ class Detector:
                     label = classNames[class_id] + ": " + str(confidence)
                     labelSize, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
                     yLeftBottom = max(yLeftBottom, labelSize[1])
-                    cv.putText(img, label, (xLeftBottom+5, yLeftBottom), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255))
+                    cv.putText(img, label, (xLeftBottom+5, yLeftBottom), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0))
 
         img = cv.imencode('.jpg', img)[1].tobytes()
         return img
